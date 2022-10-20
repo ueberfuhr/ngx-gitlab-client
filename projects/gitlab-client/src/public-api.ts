f1b52d458a39ddca84afdc72895b8a33239f336d
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-20 10:05:56 +0200

    Fix Publix API

--- projects/gitlab-client/src/public-api.ts
+++ projects/gitlab-client/src/public-api.ts
@@ -3,4 +3,6 @@
  */
 
 export * from './lib/gitlab-client.module';
+export * from './lib/config/gitlab-config.model';
+export * from './lib/shared/gitlab.service';
 export {LabelsByNamePipe} from './lib/services/issues/labels-by-name.pipe';
